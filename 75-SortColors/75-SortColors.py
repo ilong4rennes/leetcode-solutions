3422ddb10a1bef41f45b437f2e0ac2042b4f1c93
Amanda Lu <amanda.yichenlu@gmail.com>
2024-11-30 10:54:33 +0800

    Update file 11/29/2024, 9:54:32 PM

--- 75-SortColors/75-SortColors.py
+++ 75-SortColors/75-SortColors.py
@@ -1,39 +1,40 @@
 class Solution:
-     def sortColors(self, nums):
-        def merge_sort(arr, start, end):
-            if end - start <= 1:
-                return
-            
-            mid = (start + end) // 2
-            merge_sort(arr, start, mid)
-            merge_sort(arr, mid, end)
-            merge(arr, start, mid, end)
-        
-        def merge(arr, start, mid, end):
-            left = arr[start:mid]
-            right = arr[mid:end]
-            i = j = 0
-            k = start
-            
-            while i < len(left) and j < len(right):
-                if left[i] <= right[j]:
-                    arr[k] = left[i]
-                    i += 1
-                else:
-                    arr[k] = right[j]
-                    j += 1
-                k += 1
-            
-            while i < len(left):
-                arr[k] = left[i]
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        start = 0
+        end = len(nums)
+        return self.mergeSort(nums, start, end)
+
+    def mergeSort(self, nums, start, end):
+        if end - start <= 1: return
+        mid = (start + end) // 2
+        self.mergeSort(nums, start, mid)
+        self.mergeSort(nums, mid, end)
+        self.merge(nums, start, mid, end)
+    
+    def merge(self, nums, start, mid, end):
+        left = nums[start:mid]
+        right = nums[mid:end]
+        i = j = 0
+        k = start 
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
                 i += 1
-                k += 1
-            
-            while j < len(right):
-                arr[k] = right[j]
+            else: 
+                nums[k] = right[j]
                 j += 1
-                k += 1
+            k += 1
         
-        # Apply merge sort on the entire array
-        merge_sort(nums, 0, len(nums))
+        while i < len(left):
+            nums[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            nums[k] = right[j]
+            j += 1
+            k += 1
         
