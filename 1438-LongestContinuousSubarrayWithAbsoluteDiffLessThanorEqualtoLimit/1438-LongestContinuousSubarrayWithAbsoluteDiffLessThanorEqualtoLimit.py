f291a61d639391ba290ebbfc4054ff8f80234521
Amanda Lu <amanda.yichenlu@gmail.com>
2024-12-26 17:35:44 +0900

    Update file 12/26/2024, 5:35:44 PM

--- 1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit/1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit.py
+++ 1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit/1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit.py
@@ -1,24 +1,22 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
         min_deque, max_deque = deque(), deque()
-        l = r = 0
-        ans = 0
-        while r < len(nums):
-            while min_deque and nums[r] <= nums[min_deque[-1]]:
+        left = 0
+        result = 0
+        for right in range(len(nums)):
+            while min_deque and min_deque[-1] > nums[right]:
                 min_deque.pop()
-            while max_deque and nums[r] >= nums[max_deque[-1]]:
+            min_deque.append(nums[right])
+            while max_deque and max_deque[-1] < nums[right]:
                 max_deque.pop()
-            min_deque.append(r)
-            max_deque.append(r)
-            
-            while nums[max_deque[0]] - nums[min_deque[0]] > limit:
-                l += 1
-                if l > min_deque[0]:
-                    min_deque.popleft()
-                if l > max_deque[0]:
+            max_deque.append(nums[right])
+
+            while max_deque[0] - min_deque[0] > limit:
+                if max_deque[0] == nums[left]: 
                     max_deque.popleft()
-            
-            ans = max(ans, r - l + 1)
-            r += 1
-                
-        return ans
+                if min_deque[0] == nums[left]: 
+                    min_deque.popleft()
+                left += 1
+            result = max(result, right - left + 1)
+
+        return result
