705f399c67adfcac5e48612648b9f719fa62b80e
Amanda Lu <amanda.yichenlu@gmail.com>
2024-12-31 03:04:25 +0800

    Update 692-TopKFrequentWords.py

--- 692-TopKFrequentWords/692-TopKFrequentWords.py
+++ 692-TopKFrequentWords/692-TopKFrequentWords.py
@@ -1,14 +1,33 @@
-from heapq import heappush, heappop
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+        self.count = 0
+
+    def insert(self, word):
+        node = self
+        for c in word:
+            if c not in node.children:
+                node.children[c] = Trie()
+            node = node.children[c]
+        node.is_end = True
+        node.count += 1
+    
+    def words_freq(self, prefix, result):
+        node = self
+        if node.is_end:
+            result.append((prefix, node.count))
+            # prefix = ''
+        for (char, child_node) in node.children.items():
+            new_prefix = prefix + char
+            child_node.words_freq(new_prefix, result)
+        return result
 
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        wordsCounter = Counter(words)
-        wordsHeap = []
-        for word, freq in wordsCounter.items():
-            heappush(wordsHeap, (-freq, word))
-            
-        res = []
-        for _ in range(k):
-            res.append(heapq.heappop(wordsHeap)[1])
-            
-        return res
+        trie = Trie()
+        for word in words:
+            trie.insert(word)
+        freq_list = trie.words_freq('', [])
+        sorted_words = sorted(freq_list, key=lambda x: (-x[1], x[0]))
+        return [word for word, freq in sorted_words[:k]]
