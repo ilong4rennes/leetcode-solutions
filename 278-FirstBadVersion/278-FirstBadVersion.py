eec908e48020e6a9b2d8ff5c02438e32a363f1ba
Amanda Lu <amanda.yichenlu@gmail.com>
2025-01-05 21:19:38 +0800

    Update file 1/5/2025, 9:19:38 PM

--- 278-FirstBadVersion/278-FirstBadVersion.py
+++ 278-FirstBadVersion/278-FirstBadVersion.py
@@ -3,13 +3,12 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        if n == 1: return 1
         low, high = 1, n 
         while low <= high:
             mid = (low + high) // 2
             if isBadVersion(mid) == True:
                 high = mid - 1
-                if mid == 0 or isBadVersion(mid - 1) == False:
+                if isBadVersion(mid - 1) == False:
                     return mid 
             else:
                 low = mid + 1
