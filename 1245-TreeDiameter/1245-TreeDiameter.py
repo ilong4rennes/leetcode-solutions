fae1434062216ebd1ea779b1ed0c22539e2db22d
Amanda Lu <amanda.yichenlu@gmail.com>
2025-01-07 18:02:39 +0800

    Update 1245-TreeDiameter.py

--- 1245-TreeDiameter/1245-TreeDiameter.py
+++ 1245-TreeDiameter/1245-TreeDiameter.py
@@ -1,1 +1,32 @@
-bfs = {(v, u) for u, pre in bfs for v in graph[u] if v != pre}
+class Solution:
+    def treeDiameter(self, edges: List[List[int]]) -> int:
+        # build the graph
+        graph = defaultdict(list)
+        for node1, node2 in edges:
+            graph[node1].append(node2)
+            graph[node2].append(node1)
+        
+        # use bfs to find one end of the longest path
+        node_a, dist = self.bfs(graph, 0)
+        # pass one end into bfs again to find the diameter
+        node_b, diameter = self.bfs(graph, node_a)
+        return diameter
+        
+    def bfs(self, graph, start):
+        queue = deque([(start, 0)])
+        visited = set()
+        visited.add(start)
+        maxDist = 0
+        while queue:
+            node, dist = queue.popleft()
+            maxDist = max(dist, maxDist)
+            length = len(graph[node])
+            index = 0
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    queue.append((neighbor, dist + 1))
+                    visited.add(neighbor)
+                else: 
+                    index += 1
+            if index == length and not queue:
+                return node, maxDist
