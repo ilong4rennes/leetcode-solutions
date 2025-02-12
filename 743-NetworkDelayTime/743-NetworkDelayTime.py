3b1d08687b518ee8fdad4694df300a1029acdd6a
Amanda Lu <amanda.yichenlu@gmail.com>
2025-02-12 01:02:50 -0500

    File updated at 2/12/2025, 1:02:50 AM

--- 743-NetworkDelayTime/743-NetworkDelayTime.py
+++ 743-NetworkDelayTime/743-NetworkDelayTime.py
@@ -1,24 +1,23 @@
 class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        graph = defaultdict(list)
-        for u, v, w in times:
-            graph[u].append((v, w))
-        
-        distance = dict()
-        for i in range(1, n + 1):
-            distance[i] = float(inf)
-        distance[k] = 0
-    
-        visited = deque()
-        visited.append((0, k))
+    def networkDelayTime(self, times: List[List[int]], n: int, source: int) -> int:
+        distances = [float('inf')] * (n + 1)
+        distances[source] = 0
+        pq = [(0, source)] # (distance, node)
+        heapq.heapify(pq)
+        adj_list = self.transform_graph(times)
 
-        while visited:
-            curr_dist, node = visited.popleft()
-            for next_node, weight in graph[node]:
-                new_dist = curr_dist + weight
-                if new_dist < distance[next_node]:
-                    distance[next_node] = new_dist
-                    visited.append((new_dist, next_node))
-        
-        max_dist = max(distance.values())
-        return max_dist if max_dist < float('inf') else -1
+        while pq:
+            curr_dist, curr_node = heapq.heappop(pq)
+            if curr_dist > distances[curr_node]: continue
+            for neighbor, weight in adj_list[curr_node]:
+                new_dist = distances[curr_node] + weight
+                if new_dist < distances[neighbor]:
+                    distances[neighbor] = new_dist
+                    heapq.heappush(pq, (new_dist, neighbor))
+        return max(distances[1:]) if max(distances[1:]) != float('inf') else -1
+    
+    def transform_graph(self, times):
+        adj_list = defaultdict(list)
+        for source, target, weight in times:
+            adj_list[source].append((target, weight))
+        return adj_list
