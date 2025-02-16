c622d28a0d60dc1b68cd4131edd4656ff84e3ccc
Amanda Lu <amanda.yichenlu@gmail.com>
2025-02-16 16:04:45 -0500

    File updated at 2/16/2025, 3:04:45 PM

--- 684-RedundantConnection/684-RedundantConnection.py
+++ 684-RedundantConnection/684-RedundantConnection.py
@@ -1,23 +1,39 @@
 class Solution:
-    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        adj_list = defaultdict(list)
-        for a, b in edges:
-            if self.foundCycle(adj_list, a, b, None):  
-                return [a, b] 
+    def findRedundantConnection(self, edges):
+        def getCycleStartWithParents(adj_list):
+            parents = [-1] * N
+            visited = [False] * N
+            stack = [0]
+            while (stack):
+                src = stack.pop()
+                visited[src] = True
+                for nxt in adj_list[src]:
+                    if not visited[nxt]:
+                        parents[nxt] = src
+                        stack.append(nxt)
+                    if visited[nxt] and nxt != parents[src]:
+                        parents[nxt] = src
+                        return nxt, parents
+            return -1, parents
 
-            adj_list[a].append(b)
-            adj_list[b].append(a)
+        N = len(edges)
+        adj_list = [[] for _ in range(N)]
+        for edge in edges:
+            adj_list[edge[0] - 1].append(edge[1] - 1)
+            adj_list[edge[1] - 1].append(edge[0] - 1)
 
-        return []
+        node, parents = getCycleStartWithParents(adj_list)
+        node_copy = node
 
-    def foundCycle(self, adj_list, node, target, parent):
-        """DFS to check if there's a path from node to target, ignoring the parent."""
-        if node == target:
-            return True  # Cycle found
-        
-        for neighbor in adj_list[node]:
-            if (neighbor != parent and 
-                self.foundCycle(adj_list, neighbor, target, node)):
-                    return True
-        
-        return False
+        node_cycle = set()
+        while True:
+            node_cycle.add(node)
+            node = parents[node]
+            if node == node_copy:
+                break
+
+        for i in range(len(edges) - 1, -1, -1):
+            if (edges[i][0] - 1) in node_cycle and (edges[i][1] - 1) in node_cycle:
+                return edges[i]
+
+        return -1
