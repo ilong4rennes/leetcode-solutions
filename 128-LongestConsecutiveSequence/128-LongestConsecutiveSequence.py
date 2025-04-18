57856395d2c679a14c2ad54febd9c24947a6bd6b
Amanda Lu <amanda.yichenlu@gmail.com>
2025-04-18 04:43:50 -0400

    File updated at 4/18/2025, 4:43:49 AM

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.py
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.py
@@ -1,33 +1,15 @@
-# Last updated: 4/18/2025, 4:42:55 AM
-# class Solution:
-#     def longestConsecutive(self, nums: List[int]) -> int:
-#         if nums == []: return 0
-#         numset = set(nums)
-#         maxLength = 1
-#         for num in nums:
-#             if num - 1 not in numset:
-#                 currLength = 1
-#                 while num + 1 in numset:
-#                     num += 1
-#                     currLength += 1
-#                 maxLength = max(currLength, maxLength)
-
-#         return maxLength
-
+# Last updated: 4/18/2025, 4:43:49 AM
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        longest_streak = 0
-        num_set = set(nums)
+        if nums == []: return 0
+        numset = set(nums)
+        maxLength = 1
+        for num in numset:
+            if num - 1 not in numset:
+                currLength = 1
+                while num + 1 in numset:
+                    num += 1
+                    currLength += 1
+                maxLength = max(currLength, maxLength)
 
-        for num in num_set:
-            if num - 1 not in num_set:
-                current_num = num
-                current_streak = 1
-
-                while current_num + 1 in num_set:
-                    current_num += 1
-                    current_streak += 1
-
-                longest_streak = max(longest_streak, current_streak)
-
-        return longest_streak
+        return maxLength
