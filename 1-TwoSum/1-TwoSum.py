a0c91e66825866eb92f3e69bd30cb41c7b6af0b5
Amanda Lu <amanda.yichenlu@gmail.com>
2025-04-24 18:53:23 -0400

    File updated at 4/24/2025, 6:53:23 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,11 +1,18 @@
+# Last updated: 4/24/2025, 6:53:23 PM
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        record = dict()
-        for index in range(len(nums)):
-            if (target - nums[index]) in record.keys():
-                result = []
-                result.append(index)
-                result.append(record.get(target - nums[index], 0))
-                return result
+        # Pair each number with its original index
+        nums_indexed = [(num, i) for i, num in enumerate(nums)]
+        nums_indexed.sort(key=lambda x: x[0])
+
+        left, right = 0, len(nums_indexed) - 1
+        while left < right:
+            s = nums_indexed[left][0] + nums_indexed[right][0]
+            if s == target:
+                return [nums_indexed[left][1], nums_indexed[right][1]]
+            elif s < target:
+                left += 1
             else:
-                record[nums[index]] = index
+                right -= 1
+        # If no solution found (problem guarantees one), you could raise or return []
+        return []
