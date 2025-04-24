c16b476b9369e0b05708930f3e965f7a211aaaeb
Amanda Lu <amanda.yichenlu@gmail.com>
2025-04-24 19:45:33 -0400

    File updated at 4/24/2025, 7:45:33 PM

--- 75-SortColors/75-SortColors.py
+++ 75-SortColors/75-SortColors.py
@@ -1,40 +1,20 @@
+# Last updated: 4/24/2025, 7:45:33 PM
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        start = 0
-        end = len(nums)
-        return self.mergeSort(nums, start, end)
-
-    def mergeSort(self, nums, start, end):
-        if end - start <= 1: return
-        mid = (start + end) // 2
-        self.mergeSort(nums, start, mid)
-        self.mergeSort(nums, mid, end)
-        self.merge(nums, start, mid, end)
-    
-    def merge(self, nums, start, mid, end):
-        left = nums[start:mid]
-        right = nums[mid:end]
-        i = j = 0
-        k = start 
-
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                nums[k] = left[i]
-                i += 1
-            else: 
-                nums[k] = right[j]
-                j += 1
-            k += 1
-        
-        while i < len(left):
-            nums[k] = left[i]
-            i += 1
-            k += 1
-        while j < len(right):
-            nums[k] = right[j]
-            j += 1
-            k += 1
-        
+        left, right, curr = 0, len(nums) - 1, 0
+        while curr <= right:
+            leftElem, rightElem, currElem = nums[left], nums[right], nums[curr]
+            if currElem == 0:
+                nums[left] = currElem
+                nums[curr] = leftElem
+                left += 1
+                curr += 1
+            elif currElem == 1:
+                curr += 1
+            else: # currElem == 2
+                nums[right] = currElem
+                nums[curr] = rightElem
+                right -= 1
