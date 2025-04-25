165deb9a59badde4faaffe01720c66b340635a55
Amanda Lu <amanda.yichenlu@gmail.com>
2025-04-24 22:33:17 -0400

    File updated at 4/24/2025, 10:33:17 PM

--- 219-ContainsDuplicateII/219-ContainsDuplicateII.py
+++ 219-ContainsDuplicateII/219-ContainsDuplicateII.py
@@ -1,11 +1,9 @@
-# Last updated: 4/24/2025, 10:33:01 PM
+# Last updated: 4/24/2025, 10:33:17 PM
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         id_dict = {}
         for index, num in enumerate(nums):
-            print(index, num)
             last_id = id_dict.get(num, -1)
-            print("last id, ", last_id)
             if last_id != -1:
                 if abs(index - last_id) <= k:
                     return True
