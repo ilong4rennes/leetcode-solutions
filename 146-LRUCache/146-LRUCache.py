b7556217c7f51db0e52e4ce7ef2dbfd4f9a8066a
Amanda Lu <amanda.yichenlu@gmail.com>
2025-05-06 06:04:20 -0400

    Create file

---
+++ 146-LRUCache/146-LRUCache.py
@@ -0,0 +1,43 @@
+# Last updated: 5/6/2025, 6:04:19 PM
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.capacity = capacity
+        self.head = {}
+        self.tail = {}
+        self.head['next'] = self.tail
+        self.tail['prev'] = self.head
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add(node)
+            return node['val']
+        else: return -1    
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+        node = {'key': key, 'val': value}
+        self._add(node)
+        self.cache[key] = node
+        if len(self.cache) > self.capacity:
+            head = self.head['next']
+            self._remove(head)
+            del self.cache[head['key']]
+    
+    def _remove(self, node):
+        prev = node['prev']
+        next = node['next']
+        prev['next'] = next
+        next['prev'] = prev
+    
+    def _add(self, node):
+        prev = self.tail['prev']
+        prev['next'] = node
+        self.tail['prev'] = node
+        node['prev'] = prev
+        node['next'] = self.tail
+        
