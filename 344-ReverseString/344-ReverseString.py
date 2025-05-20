57b6ffdbb9b3aa4ebb6db73f288105e59dae15e4
Amanda Lu <amanda.yichenlu@gmail.com>
2025-05-20 16:38:42 +0800

    Create file

---
+++ 344-ReverseString/344-ReverseString.py
@@ -0,0 +1,14 @@
+# Last updated: 5/20/2025, 4:38:41 PM
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left, right = 0, len(s) - 1
+        while left < right:
+            temp = s[right]
+            s[right] = s[left]
+            s[left] = temp
+            left += 1
+            right -= 1
+        return s
