4e9738e098eb58b9112010f92185c5c26103308e
Amanda Lu <amanda.yichenlu@gmail.com>
2025-05-27 00:15:17 +0800

    File updated at 5/27/2025, 12:15:16 AM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
@@ -1,4 +1,4 @@
-# Last updated: 5/26/2025, 11:58:57 PM
+# Last updated: 5/27/2025, 12:15:16 AM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,17 +6,13 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        depth = 0
-        self.result = 0
-        self.traverse(depth, root)
-        return self.result 
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        self.result = []
+        self.traverse(root)
+        return self.result
     
-    def traverse(self, depth, root):
-        if root is None: return
-        depth += 1
-        if root.left is None and root.right is None:
-            self.result = max(depth, self.result)
-        self.traverse(depth, root.left)
-        self.traverse(depth, root.right)
-        depth -= 1
+    def traverse(self, node):
+        if not node: return
+        self.result.append(node.val)
+        self.traverse(node.left)
+        self.traverse(node.right)
