aee11a27ea44ecd3e9953d5bb03b20490313cfd5
Amanda Lu <amanda.yichenlu@gmail.com>
2025-06-02 15:53:41 +0800

    File updated at 6/2/2025, 3:53:40 PM

--- 106-ConstructBinaryTreefromInorderandPostorderTraversal/106-ConstructBinaryTreefromInorderandPostorderTraversal.py
+++ 106-ConstructBinaryTreefromInorderandPostorderTraversal/106-ConstructBinaryTreefromInorderandPostorderTraversal.py
@@ -1,3 +1,4 @@
+# Last updated: 6/2/2025, 3:53:40 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -5,11 +6,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.val2ind = dict()
+
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-        if not inorder: return 
-        root_val = postorder.pop()
-        root = TreeNode(root_val, None, None)
-        index = inorder.index(root_val)
-        root.right = self.buildTree(inorder[index+1:], postorder)
-        root.left = self.buildTree(inorder[:index], postorder)
+        for i in range(len(inorder)):
+            self.val2ind[inorder[i]] = i
+        return self.build(inorder, 0, len(inorder) - 1, postorder, 0, len(postorder) - 1)
+    
+    def build(self, inorder, inStart, inEnd, postorder, postStart, postEnd):
+        if inStart > inEnd or postStart > postEnd: return 
+        rootVal = postorder[postEnd]
+        root = TreeNode(rootVal)
+        rootId = self.val2ind[rootVal]
+        leftLen = rootId - inStart
+        root.left = self.build(inorder, inStart, inStart + leftLen - 1,
+                               postorder, postStart, postStart + leftLen - 1)
+        root.right = self.build(inorder, rootId + 1, inEnd, 
+                                postorder, postStart + leftLen, postEnd - 1)
         return root
