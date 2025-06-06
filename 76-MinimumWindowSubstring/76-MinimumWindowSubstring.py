34e99ed826bf0de41fa39b73e480a2d9d946973e
Amanda Lu <amanda.yichenlu@gmail.com>
2025-06-07 03:02:30 +0800

    File updated at 6/6/2025, 3:02:30 PM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
@@ -1,30 +1,32 @@
-# Last updated: 6/6/2025, 3:01:38 PM
+# Last updated: 6/6/2025, 3:02:30 PM
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        need, window = {}, {}
-        valid = 0
+        window, need = {}, {}
+        for c in t:
+            need[c] = need.get(c, 0) + 1
         left, right = 0, 0
+        valid = 0
         length = float('inf')
-
-        for char in t:
-            need[char] = need.get(char, 0) + 1
-
+        start = 0
         while right < len(s):
             c = s[right]
             right += 1
-            if c in need:
+            # update window
+            if c in need: 
                 window[c] = window.get(c, 0) + 1
-                if window.get(c, 0) == need[c]:
+                if window[c] == need[c]: 
                     valid += 1
-            
             while left < right and valid == len(need):
+                # update the minimum substring
                 if right - left < length:
                     start = left
                     length = right - left
                 d = s[left]
                 left += 1
-                if d in need:
-                    if window.get(d, 0) == need[d]:
+                # update window
+                if d in need: 
+                    # window[d] -= 1
+                    if window[d] == need[d]: 
                         valid -= 1
                     window[d] -= 1
-        return "" if length == float('inf') else s[start: start + length]
+        return '' if length == float('inf') else s[start : start + length]
