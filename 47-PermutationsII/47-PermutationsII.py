4aab37680ce99271224e42b9c9b2302628f06ce0
Amanda Lu <amanda.yichenlu@gmail.com>
2025-07-16 01:33:38 -0400

    File updated at 7/16/2025, 1:33:37 AM

--- 47-PermutationsII/47-PermutationsII.py
+++ 47-PermutationsII/47-PermutationsII.py
@@ -1,17 +1,22 @@
+# Last updated: 7/16/2025, 1:33:37 AM
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         self.result = []
-        self.visited = [False] * len(nums)
-        self.backtrack([], nums)
+        self.track = []
+        self.used = [False] * len(nums)
+        nums.sort()
+        self.backtrack(nums)
         return self.result
-        
-    def backtrack(self, path, nums):
-        if len(path) == len(nums) and path[:] not in self.result:
-            self.result.append(path[:])
-        for i in range(len(nums)):
-            if not self.visited[i]:
-                self.visited[i] = True
-                path.append(nums[i])
-                self.backtrack(path, nums)
-                path.pop()
-                self.visited[i] = False
+    
+    def backtrack(self, nums):
+        if len(self.track) == len(nums):
+            self.result.append(self.track.copy())
+        for i in range(0, len(nums)):
+            if self.used[i]: continue
+            if i > 0 and nums[i] == nums[i - 1] and not self.used[i - 1]: continue
+
+            self.track.append(nums[i])
+            self.used[i] = True
+            self.backtrack(nums)
+            self.used[i] = False
+            self.track.pop()
