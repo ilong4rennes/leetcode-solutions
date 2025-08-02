e5ea6097d2bbb4e8eb61ccfbdca7d4cf2a91a548
Amanda Lu <amanda.yichenlu@gmail.com>
2025-08-02 07:12:44 -0400

    File updated at 8/2/2025, 7:12:44 AM

--- 39-CombinationSum/39-CombinationSum.py
+++ 39-CombinationSum/39-CombinationSum.py
@@ -1,21 +1,19 @@
-# Last updated: 7/30/2025, 7:01:31 AM
+# Last updated: 8/2/2025, 7:12:44 AM
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         self.result = []
         self.track = []
-        self.trackSum = 0
-        self.backtrack(candidates, target, 0)
-        return self.result
+        start = 0
+        self.backtrack(start, candidates, target)
+        return self.result 
     
-    def backtrack(self, candidates, target, start):
-        if self.trackSum == target:
+    def backtrack(self, start, candidates, target):
+        if sum(self.track) == target:
             self.result.append(self.track.copy())
             return
-        if self.trackSum > target:
+        if sum(self.track) > target:
             return
         for i in range(start, len(candidates)):
             self.track.append(candidates[i])
-            self.trackSum += candidates[i]
-            self.backtrack(candidates, target, i)
-            self.trackSum -= candidates[i]
+            self.backtrack(i, candidates, target)
             self.track.pop()
