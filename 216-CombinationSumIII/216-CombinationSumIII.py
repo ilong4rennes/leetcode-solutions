1d22d84ced5637b62ba5b886cca95b56456d1770
Amanda Lu <amanda.yichenlu@gmail.com>
2025-08-02 08:24:40 -0400

    File updated at 8/2/2025, 8:24:40 AM

--- 216-CombinationSumIII/216-CombinationSumIII.py
+++ 216-CombinationSumIII/216-CombinationSumIII.py
@@ -1,18 +1,20 @@
+# Last updated: 8/2/2025, 8:24:40 AM
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        options = [i for i in range(1, 10)]
-        result = []
-        self.backtracking([], result, options, k, n, 0)
-        return result
+        candidates = [i for i in range(1, 10)]
+        self.result = []
+        self.track = []
+        start = 0
+        self.backtrack(start, candidates, n, k)
+        return self.result
     
-    def backtracking(self, path, result, options, k, target, startIndex):
-        if sum(path) == target and len(path) == k:
-            result.append(path[:])
+    def backtrack(self, start, candidates, target, k):
+        if sum(self.track) == target and len(self.track) == k:
+            self.result.append(self.track.copy())
             return
-        elif sum(path) > target:
+        if sum(self.track) > target:
             return
-        for i in range(startIndex, len(options)):
-            path.append(options[i])
-            if len(path) <= k:
-                self.backtracking(path, result, options, k, target, i + 1)
-            path.pop()
+        for i in range(start, len(candidates)):
+            self.track.append(candidates[i])
+            self.backtrack(i + 1, candidates, target, k)
+            self.track.pop()
