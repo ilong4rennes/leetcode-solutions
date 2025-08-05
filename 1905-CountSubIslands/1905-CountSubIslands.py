0f6e0e7aa1887a9ba0b666b637e4f9460e187b7a
Amanda Lu <amanda.yichenlu@gmail.com>
2025-08-05 04:48:58 -0400

    Create 1905-CountSubIslands

---
+++ 1905-CountSubIslands/1905-CountSubIslands.py
@@ -0,0 +1,29 @@
+# Last updated: 8/5/2025, 4:48:58 AM
+class Solution:
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        rows, cols = len(grid1), len(grid1[0])
+        for row in range(rows):
+            for col in range(cols):
+                if grid2[row][col] == 1 and grid1[row][col] == 0: 
+                    self.dfs(row, col, grid2)
+
+        result = 0 
+        for row in range(rows):
+            for col in range(cols):
+                if grid2[row][col] == 1:
+                    result += 1
+                    self.dfs(row, col, grid2)
+        
+        return result
+    
+    def dfs(self, row, col, grid):
+        rows, cols = len(grid), len(grid[0])
+        if row < 0 or row >= rows or col < 0 or col >= cols:
+            return
+        if grid[row][col] == 0:
+            return
+        grid[row][col] = 0
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        for dr, dc in directions:
+            newRow, newCol = row + dr, col + dc
+            self.dfs(newRow, newCol, grid)
