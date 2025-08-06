a6ce5e968193d02c6905f1f541f654e4f82bf8d4
Amanda Lu <amanda.yichenlu@gmail.com>
2025-08-05 20:28:58 -0400

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -0,0 +1,12 @@
+# Last updated: 8/5/2025, 8:28:58 PM
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x > 0 and x%10 == 0):   # if x is negative, return False. if x is positive and last digit is 0, that also cannot form a palindrome, return False.
+            return False
+        
+        result = 0
+        while x > result:
+            result = result * 10 + x % 10
+            x = x // 10
+            
+        return True if (x == result or x == result // 10) else False 
