ea7f6159defbb1bfe350f4114f0036385332593e
amandalu <amanda.yichenlu@gmail.com>
2025-10-04 16:54:42 -0400

    File updated at 10/4/2025, 4:54:42 PM

--- 743-NetworkDelayTime/743-NetworkDelayTime.py
+++ 743-NetworkDelayTime/743-NetworkDelayTime.py
@@ -1,23 +1,38 @@
+# Last updated: 10/4/2025, 4:54:42 PM
 class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, source: int) -> int:
-        distances = [float('inf')] * (n + 1)
-        distances[source] = 0
-        pq = [(0, source)] # (distance, node)
-        heapq.heapify(pq)
-        adj_list = self.transform_graph(times)
+    class State:
+        def __init__(self, node, distFromStart):
+            self.node = node
+            self.distFromStart = distFromStart
+        
+        def __lt__(self, other):
+            return self.distFromStart < other.distFromStart
 
+    def dijkstra(self, graph, src):
+        distTo = [-1] * len(graph)
+        pq = []
+        heapq.heappush(pq, self.State(src, 0))
+        
         while pq:
-            curr_dist, curr_node = heapq.heappop(pq)
-            if curr_dist > distances[curr_node]: continue
-            for neighbor, weight in adj_list[curr_node]:
-                new_dist = distances[curr_node] + weight
-                if new_dist < distances[neighbor]:
-                    distances[neighbor] = new_dist
-                    heapq.heappush(pq, (new_dist, neighbor))
-        return max(distances[1:]) if max(distances[1:]) != float('inf') else -1
-    
-    def transform_graph(self, times):
-        adj_list = defaultdict(list)
-        for source, target, weight in times:
-            adj_list[source].append((target, weight))
-        return adj_list
+            state = heapq.heappop(pq)
+            curNode = state.node
+            curDistFromStart = state.distFromStart
+            if distTo[curNode] != -1: continue
+            distTo[curNode] = curDistFromStart
+            for neighbor in graph[curNode]:
+                nextNode = neighbor[0]
+                nextDistFromStart = curDistFromStart + neighbor[1]
+                if distTo[nextNode] != -1: continue
+                heapq.heappush(pq, self.State(nextNode, nextDistFromStart))
+        return distTo
+
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = [[] for _ in range(n + 1)]
+        for _from, _to, weight in times:
+            graph[_from].append([_to, weight])
+        distTo = self.dijkstra(graph, k)
+        for i in range(1, n + 1):
+            if distTo[i] == -1: return -1
+        return max(distTo)
+
+        
