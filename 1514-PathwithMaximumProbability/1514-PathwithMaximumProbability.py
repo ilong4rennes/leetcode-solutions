8800c615a1fbc71a185ef8e90ece9da18aa841cb
amandalu <amanda.yichenlu@gmail.com>
2025-10-05 18:35:56 -0400

    File updated at 10/5/2025, 6:35:56 PM

--- 1514-PathwithMaximumProbability/1514-PathwithMaximumProbability.py
+++ 1514-PathwithMaximumProbability/1514-PathwithMaximumProbability.py
@@ -1,28 +1,40 @@
+# Last updated: 10/5/2025, 6:35:56 PM
 class Solution:
-    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
-        adj_list = self.transform_graph(edges, succProb)
-        probs = [0] * n
-        probs[start_node] = 1
-        pq = [(-1, start_node)] # (-probability, node)
-        heapq.heapify(pq)
-
+    class State:
+        def __init__(self, node, probFromStart):
+            self.node = node
+            self.probFromStart = probFromStart
+        
+        def __lt__(self, other):
+            return self.probFromStart > other.probFromStart
+        
+    def dijkstra(self, graph, succProb, start_node, end_node):
+        probTo = [-1] * len(graph)
+        pq = []
+        heapq.heappush(pq, self.State(start_node, 1))
         while pq:
-            curr_prob, curr_node = heapq.heappop(pq)
-            curr_prob = -curr_prob
-            if curr_prob < probs[curr_node]: continue
-            for neighbor, prob in adj_list[curr_node]:
-                new_prob = curr_prob * prob
-                if new_prob > probs[neighbor]:
-                    probs[neighbor] = new_prob
-                    heapq.heappush(pq, (-new_prob, neighbor))
-        
-        return probs[end_node]
-        
-    def transform_graph(self, edges, succProb):
-        adj_list = defaultdict(list)
+            state = heapq.heappop(pq)
+            curNode = state.node
+            curProbFromStart = state.probFromStart
+            if probTo[curNode] != -1: continue
+            if curNode == end_node:
+                return curProbFromStart
+            probTo[curNode] = curProbFromStart
+            for neighbor in graph[curNode]:
+                nextNode, prob = neighbor[0], neighbor[1]
+                nextProbFromStart = curProbFromStart * prob
+                if probTo[nextNode] != -1: continue
+                heapq.heappush(pq, self.State(nextNode, nextProbFromStart))
+        return 0
+
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        graph = [[] for _ in range(n)]
         for i in range(len(edges)):
-            node1, node2 = edges[i]
-            succProbability = succProb[i]
-            adj_list[node1].append((node2, succProbability))
-            adj_list[node2].append((node1, succProbability))
-        return adj_list
+            edge = edges[i]
+            prob = succProb[i]
+            node1, node2 = edge[0], edge[1]
+            graph[node1].append((node2, prob))
+            graph[node2].append((node1, prob))
+        maxProb = self.dijkstra(graph, succProb, start_node, end_node)
+        return maxProb
+        
