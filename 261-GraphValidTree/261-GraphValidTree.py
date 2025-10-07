57c129077635d4cf0bc6bd8eb9c831a9f81157b0
amandalu <amanda.yichenlu@gmail.com>
2025-10-07 13:46:56 -0400

    File updated at 10/7/2025, 1:46:54 PM

--- 261-GraphValidTree/261-GraphValidTree.py
+++ 261-GraphValidTree/261-GraphValidTree.py
@@ -1,27 +1,40 @@
+# Last updated: 10/7/2025, 1:46:54 PM
 class Solution:
-    def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if n == 1 and not edges: return True
-        if not edges: return False
+    class UF:
+        def __init__(self, n):
+            self._count = n
+            self.parent = [i for i in range(n)]
+        
+        def union(self, p, q):
+            rootP = self.find(p)
+            rootQ = self.find(q)
+            if rootP == rootQ: return
+            self.parent[rootQ] = rootP
+            self._count -= 1
+        
+        def connected(self, p, q):
+            rootP = self.find(p)
+            rootQ = self.find(q)
+            return rootP == rootQ
+        
+        def find(self, x):
+            if self.parent[x] != x:
+                self.parent[x] = self.find(self.parent[x])
+            return self.parent[x]
 
-        adj_list = self.transform_graph(n, edges)
-        start = edges[0][0]
-        visited = set()
-        visited.add(start)
-        stack = deque()
-        stack.append((start, -1))
-        while stack:
-            currNode, parent = stack.pop()
-            for neighbor in adj_list[currNode]:
-                if neighbor not in visited:
-                    visited.add(neighbor)
-                    stack.append((neighbor, currNode))
-                elif neighbor != parent: 
-                    return False
-        return True if len(visited) == n else False
-    
-    def transform_graph(self, n, edges):
-        adj_list = defaultdict(list)
-        for start, end in edges:
-            adj_list[start].append(end)
-            adj_list[end].append(start)
-        return adj_list
+        def count(self):
+            return self._count
+
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        uf = self.UF(n)
+        for edge in edges:
+            node1, node2 = edge
+            root1, root2 = uf.find(node1), uf.find(node2)
+            if root1 != root2:
+                uf.union(node1, node2)
+            else: 
+                return False
+        return True if uf.count() == 1 else False
+
+
+        
