7297b6bdcacab32782278adc375a4c64d65444f8
amandalu <amanda.yichenlu@gmail.com>
2025-10-08 07:11:16 -0400

    File updated at 10/8/2025, 7:11:16 AM

--- 23-MergekSortedLists/23-MergekSortedLists.py
+++ 23-MergekSortedLists/23-MergekSortedLists.py
@@ -1,4 +1,4 @@
-# Last updated: 5/20/2025, 2:08:30 PM
+# Last updated: 10/8/2025, 7:11:16 AM
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -6,17 +6,28 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        dummy = ListNode(-1)
-        p = dummy
-        pq = []
-        heapq.heapify(pq)
-        for i, node in enumerate(lists):
-            if node:
-                heapq.heappush(pq, (node.val, i, node))
-        while pq:
-            minVal, i, node = heapq.heappop(pq)
-            p.next = node
+        if len(lists) == 0: return None
+        return self.mergeKLists3(lists, 0, len(lists) - 1)
+    
+    def mergeKLists3(self, lists, start, end):
+        if start == end: return lists[start]
+        mid = start + (end - start) // 2
+        left = self.mergeKLists3(lists, start, mid)
+        right = self.mergeKLists3(lists, mid + 1, end)
+        return self.merge2Lists(left, right)
+        
+    def merge2Lists(self, list1, list2):
+        p = dummy = ListNode(-1)
+        p1 = list1
+        p2 = list2
+        while p1 and p2:
+            if p1.val < p2.val:
+                p.next = p1
+                p1 = p1.next
+            else:
+                p.next = p2
+                p2 = p2.next
             p = p.next
-            if node.next:
-                heapq.heappush(pq, (node.next.val, i, node.next))
+        if p1: p.next = p1
+        if p2: p.next = p2
         return dummy.next
