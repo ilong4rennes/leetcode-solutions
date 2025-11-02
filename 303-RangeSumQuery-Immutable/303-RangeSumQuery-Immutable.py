3c00cc645bc1290e55c0c12b496e2995cb5243e3
amandalu <amanda.yichenlu@gmail.com>
2025-11-02 11:25:42 -0500

    Create 303-RangeSumQuery-Immutable

---
+++ 303-RangeSumQuery-Immutable/303-RangeSumQuery-Immutable.py
@@ -0,0 +1,17 @@
+# Last updated: 11/2/2025, 11:25:42 AM
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.preSum = [0] * (len(nums) + 1)
+        for i in range(1, len(nums) + 1):
+            self.preSum[i] = self.nums[i - 1] + self.preSum[i - 1]
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.preSum[right + 1] - self.preSum[left]
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
