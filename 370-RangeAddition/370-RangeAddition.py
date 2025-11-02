0a156e23ae1503043e76fbb975a1de08111085e3
amandalu <amanda.yichenlu@gmail.com>
2025-11-02 16:11:58 -0500

    Create 370-RangeAddition

---
+++ 370-RangeAddition/370-RangeAddition.py
@@ -0,0 +1,28 @@
+# Last updated: 11/2/2025, 4:11:57 PM
+class Diff:
+    def __init__(self, nums):
+        self.diff = [0] * len(nums)
+        self.diff[0] = nums[0]
+        for i in range(1, len(nums)):
+            self.diff[i] = nums[i] - nums[i - 1]
+    
+    def increment(self, startId, endId, inc):
+        self.diff[startId] += inc
+        if endId + 1 < len(self.diff):
+            self.diff[endId + 1] -= inc
+    
+    def result(self):
+        result = [0] * len(self.diff)
+        result[0] = self.diff[0]
+        for i in range(1, len(self.diff)):
+            result[i] = result[i - 1] + self.diff[i]
+        return result
+ 
+class Solution:
+    def getModifiedArray(self, length: int, updates: List[List[int]]) -> List[int]:
+        nums = [0] * length
+        diff = Diff(nums)
+        for update in updates:
+            startId, endId, inc = update
+            diff.increment(startId, endId, inc)
+        return diff.result()
