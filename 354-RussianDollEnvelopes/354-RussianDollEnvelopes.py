5d396460d266a7836b49e9ba2d310893627e3202
amandalu <amanda.yichenlu@gmail.com>
2026-01-08 02:37:15 +0800

    File updated at 1/8/2026, 2:37:14 AM

--- 354-RussianDollEnvelopes/354-RussianDollEnvelopes.py
+++ 354-RussianDollEnvelopes/354-RussianDollEnvelopes.py
@@ -1,25 +1,24 @@
-# Last updated: 1/8/2026, 1:52:17 AM
+# Last updated: 1/8/2026, 2:37:14 AM
 1class Solution:
 2    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
-3        n = len(envelopes)
-4        envelopes.sort(key=lambda x: (x[0], -x[1]))
-5        height = [envelopes[i][1] for i in range(n)]
-6        return self.lengthOfLIS(height)
-7        
-8    def lengthOfLIS(self, nums: List[int]) -> int:
-9        piles = 0
-10        n = len(nums)
-11        top = [0] * n
-12        for i in range(n):
-13            poker = nums[i]
-14            left, right = 0, piles
-15            while left < right:
-16                mid = (left + right) // 2
-17                if top[mid] >= poker:
-18                    right = mid
-19                else:
-20                    left = mid + 1
-21            if left == piles: 
-22                piles += 1
-23            top[left] = poker
-24        return piles
+3        envelopes.sort(key=lambda x:(x[0], -x[1]))
+4        heights = [envelopes[i][1] for i in range(len(envelopes))]
+5        return self.lengthOfLIS(heights)
+6    
+7    def lengthOfLIS(self, nums):
+8        piles = 0
+9        n = len(nums)
+10        top = [0] * n
+11        for i in range(n):
+12            poker = nums[i]
+13            left, right = 0, piles
+14            while left < right:
+15                mid = (left + right) // 2
+16                if top[mid] >= poker:
+17                    right = mid
+18                else:
+19                    left = mid + 1
+20            if left == piles:
+21                piles += 1
+22            top[left] = poker
+23        return piles
