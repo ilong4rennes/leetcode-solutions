77d002653367e429711545beb9857d21ea880b16
amandalu <amanda.yichenlu@gmail.com>
2026-01-10 02:17:32 +0800

    File updated at 1/10/2026, 2:17:32 AM

--- 931-MinimumFallingPathSum/931-MinimumFallingPathSum.py
+++ 931-MinimumFallingPathSum/931-MinimumFallingPathSum.py
@@ -1,26 +1,12 @@
-# Last updated: 1/9/2026, 6:39:31 PM
+# Last updated: 1/10/2026, 2:17:32 AM
 1class Solution:
-2    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-3        rows, cols = len(matrix), len(matrix[0])
-4        result = float('inf')
-5        self.memo = [[174181] * cols for _ in range(rows)]
-6        for col in range(cols):
-7            result = min(result, self.dp(matrix, rows - 1, col))
-8        return result
-9        
-10    def dp(self, matrix, row, col):
-11        rows, cols = len(matrix), len(matrix[0])
-12        if row < 0 or col < 0 or row >= rows or col >= cols:
-13            return 17774
-14        if row == 0:
-15            return matrix[row][col]
-16        if self.memo[row][col] != 174181:
-17            return self.memo[row][col]
-18        self.memo[row][col] = matrix[row][col] + min(
-19            self.dp(matrix, row - 1, col - 1), 
-20            min(
-21                self.dp(matrix, row - 1, col), 
-22                self.dp(matrix, row - 1, col + 1)
-23            )
-24        )
-25        return self.memo[row][col]
+2    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+3        dp = [[0] * (n + 1) for _ in range(m + 1)]
+4        for s in strs:
+5            zeros = s.count('0')
+6            ones = len(s) - zeros
+7            for i in range(m, zeros - 1, -1):
+8                for j in range(n, ones - 1, -1):
+9                    dp[i][j] = max(dp[i][j], dp[i - zeros][j - ones] + 1)
+10
+11        return dp[m][n]
