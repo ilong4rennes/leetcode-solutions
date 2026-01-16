699aaa7b4b4875c37ece0a1a4b12697dfbfcd62c
amandalu <amanda.yichenlu@gmail.com>
2026-01-16 08:44:14 +0800

    File updated at 1/15/2026, 7:44:14 PM

--- 72-EditDistance/72-EditDistance.py
+++ 72-EditDistance/72-EditDistance.py
@@ -1,25 +1,26 @@
-# Last updated: 1/15/2026, 7:23:34 PM
+# Last updated: 1/15/2026, 7:44:14 PM
 1class Solution:
 2    def minDistance(self, word1: str, word2: str) -> int:
-3        id1, id2 = len(word1) - 1, len(word2) - 1
-4        self.memo = [[-174] * len(word2) for _ in range(len(word1))]
-5        return self.dp(id1, word1, id2, word2)
-6    
-7    def dp(self, id1, word1, id2, word2):
-8        if id1 < 0: return id2 + 1
-9        if id2 < 0: return id1 + 1
-10        if self.memo[id1][id2] != -174: 
-11            return self.memo[id1][id2]
-12        
-13        if word1[id1] == word2[id2]:
-14            self.memo[id1][id2] =  self.dp(id1 - 1, word1, id2 - 1, word2)
-15        else:
-16            self.memo[id1][id2] = self.min3(
-17                self.dp(id1, word1, id2 - 1, word2) + 1,
-18                self.dp(id1 - 1, word1, id2, word2) + 1,
-19                self.dp(id1 - 1, word1, id2 - 1, word2) + 1
-20            )
-21        return self.memo[id1][id2]
-22
-23    def min3(self, a, b, c):
-24        return min(a, min(b, c))
+3        # 法二 dp table：自底向上
+4        len1, len2 = len(word1), len(word2)
+5        dp = [[0] * (len2 + 1) for _ in range(len1 + 1)]
+6
+7        # Initialize with base case
+8        for i in range(len1 + 1): dp[i][0] = i
+9        for j in range(len2 + 1): dp[0][j] = j
+10
+11        for i in range(1, len1 + 1):
+12            for j in range(1, len2 + 1):
+13                if word1[i - 1] == word2[j - 1]:
+14                    dp[i][j] = dp[i - 1][j - 1]
+15                else:
+16                    dp[i][j] = self.min3(
+17                        dp[i - 1][j] + 1,
+18                        dp[i][j - 1] + 1,
+19                        dp[i - 1][j - 1] + 1
+20                    )
+21        
+22        return dp[len1][len2]
+23    
+24    def min3(self, a, b, c):
+25        return min(a, min(b, c))
