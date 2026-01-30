1d264b1f47edce8cec57dea4b765f7794703e436
amandalu <amanda.yichenlu@gmail.com>
2026-01-30 09:51:12 +0800

    File updated at 1/29/2026, 8:51:12 PM

--- 863-AllNodesDistanceKinBinaryTree/863-AllNodesDistanceKinBinaryTree.py
+++ 863-AllNodesDistanceKinBinaryTree/863-AllNodesDistanceKinBinaryTree.py
@@ -1,4 +1,4 @@
-# Last updated: 1/29/2026, 8:49:08 PM
+# Last updated: 1/29/2026, 8:51:12 PM
 1# Definition for a binary tree node.
 2# class TreeNode:
 3#     def __init__(self, x):
@@ -14,25 +14,24 @@
 13        visited = set()
 14        visited.add(target)
 15        step = 0
-16        result = []
-17        while q:
-18            size = len(q)
-19            for _ in range(size):
-20                curr = q.popleft()
-21                if step == k:
-22                    result.append(curr.val)
-23                for to in self.graph[curr]:
-24                    if to not in visited:
-25                        q.append(to)
-26                        visited.add(to)
-27            step += 1
-28        return result
-29
-30    def tree2graph(self, node, parent):
-31        if parent:
-32            self.graph[node].append(parent)
-33            self.graph[parent].append(node)
-34        if node.left:
-35            self.tree2graph(node.left, node)
-36        if node.right:
-37            self.tree2graph(node.right, node)
+16        while q:
+17            if step == k:
+18                return [node.val for node in q]
+19            size = len(q)
+20            for _ in range(size):
+21                curr = q.popleft()
+22                for to in self.graph[curr]:
+23                    if to not in visited:
+24                        q.append(to)
+25                        visited.add(to)
+26            step += 1
+27        return []
+28
+29    def tree2graph(self, node, parent):
+30        if parent:
+31            self.graph[node].append(parent)
+32            self.graph[parent].append(node)
+33        if node.left:
+34            self.tree2graph(node.left, node)
+35        if node.right:
+36            self.tree2graph(node.right, node)
