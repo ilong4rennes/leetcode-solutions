1da197b09e1bdab1a905713996ad58a9fe77ddfe
amandalu <amanda.yichenlu@gmail.com>
2026-02-03 04:03:57 +0800

    File updated at 2/2/2026, 3:03:57 PM

--- 433-MinimumGeneticMutation/433-MinimumGeneticMutation.py
+++ 433-MinimumGeneticMutation/433-MinimumGeneticMutation.py
@@ -1,10 +1,10 @@
-# Last updated: 2/2/2026, 3:03:41 PM
+# Last updated: 2/2/2026, 3:03:57 PM
 1class Solution:
 2    def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
 3        bank = set(bank)
 4        if endGene not in bank:
 5            return -1
-6            
+6
 7        q = deque([startGene])
 8        visited = set()
 9        visited.add(startGene)
@@ -16,24 +16,23 @@
 15                if currGene == endGene:
 16                    return step
 17                for to in self.getNeighbors(currGene):
-18                    print(self.getNeighbors(currGene))
-19                    if to in bank and to not in visited:
-20                        q.append(to)
-21                        visited.add(to)
-22            step += 1
-23        return -1
-24    
-25    def getNeighbors(self, gene):
-26        geneList = list(gene)
-27        chars = ['A', 'C', 'G', 'T']
-28        results = []
-29        for i in range(len(geneList)):
-30            for char in chars:
-31                if geneList[i] == char: continue
-32                else:
-33                    newGeneList = geneList.copy()
-34                    newGeneList[i] = char
-35                    results.append(''.join(newGeneList))
-36        return results
+18                    if to in bank and to not in visited:
+19                        q.append(to)
+20                        visited.add(to)
+21            step += 1
+22        return -1
+23    
+24    def getNeighbors(self, gene):
+25        geneList = list(gene)
+26        chars = ['A', 'C', 'G', 'T']
+27        results = []
+28        for i in range(len(geneList)):
+29            for char in chars:
+30                if geneList[i] == char: continue
+31                else:
+32                    newGeneList = geneList.copy()
+33                    newGeneList[i] = char
+34                    results.append(''.join(newGeneList))
+35        return results
+36
 37
-38
