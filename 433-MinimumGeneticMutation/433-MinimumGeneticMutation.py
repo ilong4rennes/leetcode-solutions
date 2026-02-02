48e6b5511c892dbb59345e9246453444a620736f
amandalu <amanda.yichenlu@gmail.com>
2026-02-03 04:03:41 +0800

    File updated at 2/2/2026, 3:03:41 PM

--- 433-MinimumGeneticMutation/433-MinimumGeneticMutation.py
+++ 433-MinimumGeneticMutation/433-MinimumGeneticMutation.py
@@ -1,36 +1,39 @@
-# Last updated: 2/2/2026, 3:03:15 PM
+# Last updated: 2/2/2026, 3:03:41 PM
 1class Solution:
 2    def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
-3        q = deque([startGene])
-4        visited = set()
-5        visited.add(startGene)
-6        bank = set(bank)
-7        step = 0
-8        while q:
-9            size = len(q)
-10            for _ in range(size):
-11                currGene = q.popleft()
-12                if currGene == endGene:
-13                    return step
-14                for to in self.getNeighbors(currGene):
-15                    print(self.getNeighbors(currGene))
-16                    if to in bank and to not in visited:
-17                        q.append(to)
-18                        visited.add(to)
-19            step += 1
-20        return -1
-21    
-22    def getNeighbors(self, gene):
-23        geneList = list(gene)
-24        chars = ['A', 'C', 'G', 'T']
-25        results = []
-26        for i in range(len(geneList)):
-27            for char in chars:
-28                if geneList[i] == char: continue
-29                else:
-30                    newGeneList = geneList.copy()
-31                    newGeneList[i] = char
-32                    results.append(''.join(newGeneList))
-33        return results
-34
-35
+3        bank = set(bank)
+4        if endGene not in bank:
+5            return -1
+6            
+7        q = deque([startGene])
+8        visited = set()
+9        visited.add(startGene)
+10        step = 0
+11        while q:
+12            size = len(q)
+13            for _ in range(size):
+14                currGene = q.popleft()
+15                if currGene == endGene:
+16                    return step
+17                for to in self.getNeighbors(currGene):
+18                    print(self.getNeighbors(currGene))
+19                    if to in bank and to not in visited:
+20                        q.append(to)
+21                        visited.add(to)
+22            step += 1
+23        return -1
+24    
+25    def getNeighbors(self, gene):
+26        geneList = list(gene)
+27        chars = ['A', 'C', 'G', 'T']
+28        results = []
+29        for i in range(len(geneList)):
+30            for char in chars:
+31                if geneList[i] == char: continue
+32                else:
+33                    newGeneList = geneList.copy()
+34                    newGeneList[i] = char
+35                    results.append(''.join(newGeneList))
+36        return results
+37
+38
