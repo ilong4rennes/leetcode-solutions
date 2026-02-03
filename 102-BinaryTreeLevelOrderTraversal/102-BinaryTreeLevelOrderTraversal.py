f40ab67224390f6c890b17695ef34c34af4268e4
amandalu <amanda.yichenlu@gmail.com>
2026-02-03 14:00:44 +0800

    File updated at 2/3/2026, 1:00:44 AM

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
@@ -1,4 +1,4 @@
-# Last updated: 1/28/2026, 3:12:06 PM
+# Last updated: 2/3/2026, 1:00:44 AM
 1# Definition for a binary tree node.
 2# class TreeNode:
 3#     def __init__(self, val=0, left=None, right=None):
@@ -10,18 +10,15 @@
 9        if not root: return []
 10        q = deque([root])
 11        result = []
-12        
-13        while q:
+12        while q:
+13            stepLevel = []
 14            size = len(q)
-15            currLevel = []
-16            for i in range(size):
-17                curr = q.popleft()
-18                currLevel.append(curr.val)
-19                if curr.left:
-20                    q.append(curr.left)
-21                if curr.right:
-22                    q.append(curr.right)
-23            result.append(currLevel)
-24        
-25        return result
-26                
+15            for _ in range(size):
+16                curr = q.popleft()
+17                stepLevel.append(curr.val)
+18                if curr.left:
+19                    q.append(curr.left)
+20                if curr.right:
+21                    q.append(curr.right)
+22            result.append(stepLevel)
+23        return result
