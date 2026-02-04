f60ac4c11aa31f937628573b620f018adcd69355
amandalu <amanda.yichenlu@gmail.com>
2026-02-04 20:56:31 +0800

    File updated at 2/4/2026, 7:56:31 AM

--- 107-BinaryTreeLevelOrderTraversalII/107-BinaryTreeLevelOrderTraversalII.py
+++ 107-BinaryTreeLevelOrderTraversalII/107-BinaryTreeLevelOrderTraversalII.py
@@ -1,20 +1,25 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root: return []
-        result = []
-        queue = deque([root])
-        while queue:
-            level = []
-            for _ in range(len(queue)):
-                node = queue.popleft()
-                level.append(node.val)
-                if node.left: queue.append(node.left)
-                if node.right: queue.append(node.right)
-            result.append(level)
-        return result[::-1]
+# Last updated: 2/4/2026, 7:56:31 AM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+9        if not root: return []
+10        q = deque([root])
+11        result = []
+12        while q:
+13            size = len(q)
+14            stepLevel = []
+15            for _ in range(size):
+16                curr = q.popleft()
+17                stepLevel.append(curr.val)
+18                if curr.left:
+19                    q.append(curr.left)
+20                if curr.right:
+21                    q.append(curr.right)
+22            result.append(stepLevel)
+23        return result[::-1]
+24
