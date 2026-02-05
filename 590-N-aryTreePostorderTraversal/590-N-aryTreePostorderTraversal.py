53817d028e9c307a6860e085b614c998fa720460
amandalu <amanda.yichenlu@gmail.com>
2026-02-05 23:54:53 +0800

    Create 590-N-aryTreePostorderTraversal

---
+++ 590-N-aryTreePostorderTraversal/590-N-aryTreePostorderTraversal.py
@@ -0,0 +1,20 @@
+# Last updated: 2/5/2026, 10:54:53 AM
+1"""
+2# Definition for a Node.
+3class Node:
+4    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+5        self.val = val
+6        self.children = children
+7"""
+8
+9class Solution:
+10    def postorder(self, root: 'Node') -> List[int]:
+11        if not root: return []
+12        self.result = []
+13        self.traverse(root)
+14        return self.result
+15    
+16    def traverse(self, node):
+17        for to in node.children:
+18            self.traverse(to)
+19        self.result.append(node.val)
