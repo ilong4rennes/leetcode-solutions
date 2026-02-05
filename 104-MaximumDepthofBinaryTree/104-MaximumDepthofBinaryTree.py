72545c2001a7ff51ebaef4b38b3998a3064515a6
amandalu <amanda.yichenlu@gmail.com>
2026-02-06 00:18:58 +0800

    File updated at 2/5/2026, 11:18:58 AM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
@@ -1,23 +1,22 @@
-# Last updated: 7/14/2025, 12:49:24 AM
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        self.depth = 0
-        self.result = 0
-        self.traverse(root)
-        return self.result
-        
-    def traverse(self, node):
-        if node == None:
-            return
-        self.depth += 1
-        if node.left == None and node.right == None:
-            self.result = max(self.result, self.depth)
-        self.traverse(node.left)
-        self.traverse(node.right)
-        self.depth -= 1
+# Last updated: 2/5/2026, 11:18:58 AM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def maxDepth(self, root: Optional[TreeNode]) -> int:
+9        if not root: return 0
+10        q = deque([root])
+11        maxDepth = 0
+12        while q:
+13            size = len(q)
+14            for _ in range(size):
+15                curr = q.popleft()
+16                if curr.left:
+17                    q.append(curr.left)
+18                if curr.right:
+19                    q.append(curr.right)
+20            maxDepth += 1
+21        return maxDepth
